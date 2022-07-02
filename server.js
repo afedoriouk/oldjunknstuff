15ad8abe58d08d31362ed8d40de4bb195bd97baa
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-02 16:03:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use((exo = press.urlencoded({ extended: true })));
+app.use(express.json());
+
+//express App needs this code to display static pages
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
