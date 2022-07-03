d22095dff67242c4b1faebd71dbe29214de57905
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-03 12:12:50 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,9 @@
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/oldjunknstuff', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
